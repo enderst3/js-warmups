678f573c717856355493c1643c5e11624076ed75
enderst3 <enderst3@gmail.com>
2017-10-13 16:39:29 -0700

    warmups

--- warmups.js
+++ warmups.js
@@ -164,3 +164,38 @@ function duplicateCount(text){
 
 duplicateCount(abcde)
 duplicateCount(aabBcddee)
+
+/*
+Croquet Registration
+
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members in which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input:
+
+Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Example Input:
+
+[[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
+Output:
+
+Output will consist of a list of string values stating whether the respective member is to be placed in the senior or open category.
+
+Example Output:
+
+["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+*/
+
+function playerInfo(age, handicap) {
+    if ((age >= 55)  && (handicap >= 7)) {
+        console.log('Senior')
+    } else {
+        console.log('Open')
+    }
+}
+
+playerInfo(18, 20)
+playerInfo(61, 12)
