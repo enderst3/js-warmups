e88d8f11b875e0faeb6d31b1bb547dfd1364927a
enderst3 <enderst3@gmail.com>
2017-10-15 15:15:40 -0700

    recursion warmups

--- warmups.js
+++ warmups.js
@@ -199,3 +199,23 @@ function playerInfo(age, handicap) {
 
 playerInfo(18, 20)
 playerInfo(61, 12)
+
+
+// How would you call this function repeatedly to get the whole sentence?
+
+const getSentenceFragment = (offset = 0) => {
+  const pageSize = 3
+  const sentence = [...'hello world']
+  return {
+    data: sentence.slice(offset, offset + pageSize),
+    nextPage: offset + pageSize < sentence.length ? offset + pageSize : null
+  }
+}
+
+const getSentence = (sentence, offset) => {
+  // MODIFY CODE HERE ------------
+  return [];
+  // MODIFY CODE HERE ------------
+}
+
+console.log(getSentence([], 0).join(''))
