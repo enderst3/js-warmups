ee98098813390cd7021cf6fa16985f42a0c845bd
enderst3 <enderst3@gmail.com>
2017-10-16 20:10:39 -0700

    recursion warmups

--- warmups.js
+++ warmups.js
@@ -219,3 +219,23 @@ const getSentence = (sentence, offset) => {
 }
 
 console.log(getSentence([], 0).join(''))
+
+// How would you interface with this async function to get the whole sentence?
+
+const getSentenceFragment = (offset, callback) => {
+  const pageSize = 3
+  const sentence = [...'hello world']
+  setTimeout(() => callback({
+    data: sentence.slice(offset, offset + pageSize),
+    nextPage: offset +
+        pageSize < sentence.length ? offset + pageSize : undefined
+  }), 500)
+}
+
+const getSentence = (offset, callback) => {
+  // MODIFY THE CODE HERE ------
+  return []
+  // MODIFY THE CODE HERE ------
+}
+
+getSentence(0, (sentence) => console.log(sentence))
